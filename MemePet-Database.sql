9c5c1d1fd8ef38de2a427485da8ab5801368141f
Iven Yang <iven_yang@yahoo.com>
2017-04-06 03:43:12 -0400

    Update MemePet-Database.sql

--- MemePet-Database.sql
+++ MemePet-Database.sql
@@ -1,31 +1,32 @@
 --postgreSQL for MemePet project.
 
 CREATE TABLE UserAccount(
-user_id serial PRIMARY KEY,
-username VARCHAR(20) UNIQUE NOT NULL,
+username VARCHAR(20) NOT NULL,
 password VARCHAR(256) NOT NULL,
 email VARCHAR(30),
 created_on TIMESTAMP NOT NULL,
-last_login TIMESTAMP
+last_login TIMESTAMP,
+PRIMARY KEY (`username`)
 );
 
 CREATE TABLE PetModel(
-pet_id serial PRIMARY KEY,
 pet_name VARCHAR(20) UNIQUE NOT NULL,
 import_on TIMESTAMP NOT NULL,
+PRIMARY KEY (`pet_name`)
 );
 
 CREATE TABLE HavePet(
-user_id integer NOT NULL,
-pet_id integer Not NULL,
-PRIMARY KEY(user_id, pet_id),
-FOREIGN KEY(pet_id) REFERENCES petModel(pet_id) ON DELETE CASCADE,
-FOREIGN KEY(user_id) REFERENCES userAccount(user_id) ON DELETE CASCADE
+username VARCHAR(20) NOT NULL,
+pet_name VARCHAR(20) NOT NULL,
+PRIMARY KEY(username, pet_name),
+FOREIGN KEY(pet_name) REFERENCES PetModel(pet_name) ON DELETE CASCADE,
+FOREIGN KEY(username) REFERENCES UserAccount(username) ON DELETE CASCADE
 );
 
 CREATE TABLE Location(
-user_id integer PRIMARY KEY,
+username VARCHAR(20) NOT NULL,
 latitude numeric(9,6),
 longitude numeric(9,6),
-FOREIGN KEY(user_id) REFERENCES userAccount(user_id) ON DELETE CASCADE
+PRIMARY KEY(username)
+FOREIGN KEY(username) REFERENCES UserAccount(username) ON DELETE CASCADE
 );
